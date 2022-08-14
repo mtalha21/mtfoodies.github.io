2f16e6d799ae99292359e87f11b7cc0735ae6dd1
mtalha21 <talhadawood191@gmail.com>
2022-08-14 22:15:29 +0500

    MT Foodies Web

---
+++ js/main.js
@@ -0,0 +1,49 @@
+
+
+// counter design
+document.addEventListener("DOMContentLoaded", () => {
+    function counter(id, start, end, duration){
+        let obj = document.getElementById(id),
+        current = start,
+        range = end - start,
+        increment = end > start ? 1 : -1,
+        step = Math.abs(Math.floor(duration / range)),
+        timer = setInterval(() => {
+            current += increment;
+            obj.textContent = current;
+            if(current == end){
+                clearInterval(timer);
+            }
+        }, step);
+    }
+    counter("count1", 0, 2865, 3579);
+    counter("count2", 100, 1044, 2364);
+    counter("count3", 0, 2586, 3894);
+    counter("count4", 0, 1573, 1376);
+} );
+
+
+
+// nav hide
+let navbar = document.querySelectorAll('.nav-link');
+let navCollapse = document.querySelector('.navbar-collapse.collapse');
+navbar.forEach(function(a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
+
+
+// active navbar
+let nav = document.querySelector(".navigation-wrap");
+window.onscroll = function () {
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("scroll-on");
+    }else{
+        nav.classList.remove("scroll-on");
+    }
+}
+
+
+
+
